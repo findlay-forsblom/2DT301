bdb1bbe47c472e82b7d427176862613c706f15a7
Findlay <findlay12@ymail.com>
2020-02-13 15:46:10 +0100

    test

--- app.js
+++ app.js
@@ -3,12 +3,17 @@
 const express = require('express')
 const hbs = require('express-hbs')
 const path = require('path')
-const ttn = require('ttn')
-const keys = require('keys')
+// const ttn = require('ttn')
+// const keys = require('keys')
+const dotenv = require('dotenv')
 
-const port = 8000
+const port = 3000
 
 const app = express()
+
+dotenv.config({
+  path: './.env'
+})
 
 app.engine('hbs', hbs.express4({
   defaultLayout: path.join(__dirname, 'views', 'layouts', 'default'),
@@ -31,15 +36,15 @@ app.use((err, req, res, next) => {
   res.sendFile(path.join(__dirname, 'public', '500.html'))
 })
 
-ttn.data(keys.ttn.appID, keys.ttn.accessKey)
-  .then((client) => {
-    client.on('uplink', (devID, payload) => {
-      console.log('Received uplink from ', devID)
-      console.log(payload)
-    })
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+// ttn.data(process.env.appID, process.env.accessKey)
+//   .then((client) => {
+//     client.on('uplink', (devID, payload) => {
+//       console.log('Received uplink from ', devID)
+//       console.log(payload)
+//     })
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
 
 app.listen(port, () => console.log('Server running at http://localhost:' + port))
